dfbe6e18ac6bb273f2a5d007522e623e2a5703a6
obedNuertey1 <onuertey1997@gmail.com>
2024-04-21 14:03:50 +0000

    feat: added codebase

---
+++ app/transactions/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Transactions() {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+        <div id='organisation-logo-name' className=''>
+            <div id='organisation-logo'></div>
+            <div id='organisation-name'></div>
+        </div>
+        <div>
+            <div id='account-num'></div>
+            <div id='icons'></div>
+        </div>
+    </div>
+  )
+}
