7c3ee34c79d17d66a442481f863fcf10c7bb78b3
Obed Nuertey <onuertey1997@gmail.com>
2024-05-28 06:26:26 +0000

    feat: modified loginpage for testing 3

--- app/components/SplashScreen/SplashScreen.tsx
+++ app/components/SplashScreen/SplashScreen.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { faPeopleGroup } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, {useEffect, useState} from "react";
