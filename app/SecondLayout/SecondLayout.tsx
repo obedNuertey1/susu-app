472bf0d9009623bf39602bcda655204f88ddf666
Obed Nuertey <onuertey1997@gmail.com>
2024-05-28 13:40:02 +0000

    feat: modified loginpage for testing 3

---
+++ app/SecondLayout/SecondLayout.tsx
@@ -0,0 +1,39 @@
+"use client";
+import React, {lazy, Suspense,  ComponentType, useEffect, useState} from 'react'
+import waiting from '../funcs/waiting';
+import SplashScreen from '../components/SplashScreen/SplashScreen';
+
+const FirstLayout:any = lazy(async ():Promise<{default: ComponentType<JSX.Element>}>=>{
+    await waiting(10000);
+    return await import("../FirstLayout/FirstLayout");
+  })
+
+function SecondLayout({children}:any) {
+    const [companyName, setCompanyName] = useState<string>("Msys");
+    const [image, setImage] = useState<string>("");
+    useEffect(()=>{
+        (async ()=>{
+            try {
+                const res = await fetch(`${process.env.NEXT_PUBLIC_REACT_SSR_SERVER_API}/system-settings`)
+                if (!res.ok) {
+                    throw new Error("Bad request")
+                }
+                const data = await res.json();
+                setImage(`${data.image}`);
+                setCompanyName(`${data.title}`);
+            } catch (e: any) {
+                console.log(e.message);
+            }
+        })();
+        return ()=>{};
+    }, []);
+  return (
+    <Suspense fallback={<SplashScreen companyName={companyName} imageUrl={image} />}>
+        <FirstLayout>
+            {children}
+        </FirstLayout>
+    </Suspense>
+  )
+}
+
+export default SecondLayout;
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,81 +4,37 @@ import { Inter } from 'next/font/google'
 import { ComponentType, lazy, Suspense } from 'react';
 import waiting from './funcs/waiting';
 import SplashScreen from './components/SplashScreen/SplashScreen';
+import SecondLayout from './SecondLayout/SecondLayout';
 
 // const FirstLayout:any = lazy(async ():Promise<{default: ComponentType<JSX.Element>}>=>{
 //   await waiting(10000);
 //   return await import("./FirstLayout/FirstLayout");
 // })
 
-// const inter = Inter({ subsets: ['latin'] })
-
-
-
-// export const metadata: Metadata = {
-//   title: 'Msys consult',
-//   description: 'Msys consult group page',
-// }
-
-
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode
-// }) {
-//   return (
-//     <html lang="en">
-//       <body className={inter.className}>
-//       <Suspense fallback={<SplashScreen />}>
-//           <FirstLayout>
-//             {children}
-//           </FirstLayout>
-//       </Suspense>
-//         </body>
-//     </html>
-//   )
-// }
-
-const FirstLayout: any = lazy(async (): Promise<{ default: ComponentType<JSX.Element> }> => {
-  await waiting(10000);
-  return await import("./FirstLayout/FirstLayout");
-})
-
 const inter = Inter({ subsets: ['latin'] })
+
+
 
 export const metadata: Metadata = {
   title: 'Msys consult',
   description: 'Msys consult group page',
 }
 
-export default async function RootLayout({
+
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  let companyName = "Msys";
-  let imageUrl = "";
-
-  try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_REACT_SSR_SERVER_API}/system-settings`)
-      if (!res.ok) {
-          throw new Error("Bad request")
-      }
-      const data = await res.json();
-      imageUrl = data.image;
-      companyName = data.title || "Msys";
-  } catch (e: any) {
-      console.log(e.message);
-  }
-
   return (
-      <html lang="en">
-          <body className={inter.className}>
-              <Suspense fallback={<SplashScreen companyName={companyName} imageUrl={imageUrl} />}>
-                  <FirstLayout>
-                      {children}
-                  </FirstLayout>
-              </Suspense>
-          </body>
-      </html>
+    <html lang="en">
+      <body className={inter.className}>
+      {/* <Suspense fallback={<SplashScreen />}> */}
+        <SecondLayout>
+          {children}
+        </SecondLayout>
+      {/* </Suspense> */}
+        </body>
+    </html>
   )
 }
