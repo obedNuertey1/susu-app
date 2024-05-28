5523aabf02151eaa5b71dd9895485268b2da2453
Obed Nuertey <onuertey1997@gmail.com>
2024-05-28 14:24:32 +0000

    feat: modified loginpage for testing 3

--- app/components/SplashScreen/SplashScreen.tsx
+++ app/components/SplashScreen/SplashScreen.tsx
@@ -1,32 +1,25 @@
-"use client";
+// "use client";
 import { faPeopleGroup } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, {useEffect, useState} from "react";
 import Image from "next/image";
 import styles from "./splashScreen.module.css";
 
-const SplashScreen=()=>{
+const SplashScreen= async ()=>{
 
-    const [companyName, setCompanyName] = useState<string>("Msys");
-    const [imageUrl, setImageUrl] = useState<string>("");
-    useEffect(()=>{
-        (async ()=>{
-            try{
-                const res = await fetch(`${process.env.NEXT_PUBLIC_REACT_SERVER_API}/system-settings`)
-                if(!res.ok){
-                    throw new Error("Bad request")
-                }
-                const data = await res.json();
-                setImageUrl(`${data.image}`);
-                setCompanyName(`${data.title}`);
-            }catch(e:any){
-                console.log(e.message);
-            }
-        })();
-
-        return ()=>{};
-    }, []);
-
+    let companyName = "Msys";
+    let imageUrl = "";
+    try{
+        const res = await fetch(`${process.env.NEXT_PUBLIC_REACT_SERVER_API}/system-settings`)
+        if(!res.ok){
+            throw new Error("Bad request")
+        }
+        const data = await res.json();
+        imageUrl = data.image;
+        companyName = `${data.title}`;
+    }catch(e:any){
+        console.log(e.message);
+    }
 
     return (
         <div className={`w-full h-screen flex flex-col items-center justify-center bg-project-blue`}>
@@ -48,7 +41,7 @@ const SplashScreen=()=>{
 
 }
 
-// SplashScreen.displayName = 'SplashScreen';
+SplashScreen.displayName = 'SplashScreen';
 export default SplashScreen;
 
 
