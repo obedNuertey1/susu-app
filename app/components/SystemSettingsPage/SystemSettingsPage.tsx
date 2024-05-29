620434ad32f63bbde4cdc6024a77725d1724b1ce
Obed Nuertey <onuertey1997@gmail.com>
2024-05-29 23:53:47 +0000

    fix: converted tel to text

--- app/components/SystemSettingsPage/SystemSettingsPage.tsx
+++ app/components/SystemSettingsPage/SystemSettingsPage.tsx
@@ -241,7 +241,7 @@ export default function SystemSettingsPage() {
     <div>
         {
             successMessage &&
-            <div role="alert" className={`alert alert-success fixed left-0 z-50 right-0 top-[0vh] w-[90vw] justify-self-center self-center gap-1 flex-row mx-auto -translate-y-[100%] ${styles.promptAnime}`}>
+            <div role="alert" className={`alert alert-success fixed left-0 z-50 right-0 top-[0vh] w-[90vw] justify-self-center self-center gap-1 flex-row mx-auto translate-y-[100%] ${styles.promptAnime}`}>
                 <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                 <span>{successMessage}</span>
             </div>
